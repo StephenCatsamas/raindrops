dacee161c6fd8686b42be400760031cb58ce9967
Stephen Catsamas <seca8193@gmail.com>
2023-12-02 15:39:25 +1100

    learning some more about rust

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,80 @@
-use std::os::windows;
+use std::ops::{Index,Add, Sub, Mul, Neg, Deref, DerefMut};
 
 use image::{GenericImage, Rgba, RgbaImage};
 
 
 const SIZE_Z: usize = 128;
 const SIZE_X: usize = 128;
+const a     : f64   = 0.1;
+
+
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+struct Point{
+    p : [f64;2]
+}
+
+macro_rules! point {
+    ($x:expr, $y:expr) => {
+        Point { p: [$x, $y] }
+    };
+}
+
+impl Point {
+    fn norm(self) -> f64 {
+        self.p.iter().fold(0.0f64, |s, v| s + v*v).sqrt()
+    }
+}
+
+
+impl Mul<Point> for f64 {
+    type Output = Point;
+
+    fn mul(self, other: Point) -> Point {
+        Point {
+            p: [self*other.p[0], self*other.p[1]],
+        }
+    }
+} 
+
+impl Neg for Point {
+    type Output = Point;
+
+    fn neg(self) -> Point {
+        -1.0f64 * self
+    }
+}
+
+impl Add<Point> for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            p: [self.p[0] + other.p[0], self.p[1] + other.p[1]],
+        }
+    }
+}
+
+impl Sub<Point> for Point {
+    type Output = Point;
+
+    fn sub(self, other: Point) -> Point {
+        self + -other
+    }
+}
+
+impl Index<usize> for Point {
+    type Output = f64;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.p[index]
+    }
+}
 
 struct Raindrop{
     r : f64,
-    pos : [f64; 2],
+    pos : Point,
 }
-
 
 fn main() {
     println!("Raindrops");
@@ -21,23 +85,40 @@ fn main() {
 
     let mut raindrops : Vec<Raindrop> = vec!();
 
-    step(&mut raindrops);
+    step(&mut raindrops, 0.0f64);
 
 }
 
-fn step(raindrops : &mut Vec<Raindrop>) {
+fn step(raindrops : &mut Vec<Raindrop>, f : f64) {
 
     //insert new raindrops
         //lets say 10 or something of random velocity 
 
     //move // dz/dt = -α*r, dx/dt = f(t)
-    
+    raindrops.iter_mut()
+             .for_each(
+             |drop| 
+                drop.pos = drop.pos + point![f, -a*drop.r]
+             );
+    raindrops.retain(|drop| drop.pos[0] > -1.0);
+
 
     //merge
-
+    for (i, &x) in raindrops.iter().enumerate() {
+        for &y in raindrops.iter().skip(i + 1) {
+            
+        }
+    }
 
     todo!()
+   
 }
+
+fn collision(drop_a : &Raindrop, drop_b : &Raindrop) -> bool{
+    (drop_a.pos - drop_b.pos).norm() <= drop_a.r + drop_b.r
+}
+
+
 
 fn save_window(fp : &str, window : [[u8; SIZE_X]; SIZE_Z]){
     // Create an RGBA image
